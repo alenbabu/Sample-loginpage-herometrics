f49254d4825752a560eafd254b361b9e6f02b8fb
Alen Babu <alenbabualen@gmail.com>
2023-11-10 16:06:59 +0530

    initial commit , signin page design updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login-main">
+      <div className='header'>
+        <h1 className='header-title'>Login</h1>
+      </div>
+      <div className='signin-details'>
+
+        <div className='userpass'>
+
+          <input type='email' placeholder='Email *' />
+
+          <div className='icon'>
+            <i class="fa-solid fa-user"></i>
+
+          </div>
+
+        </div>
+        <div className='userpass'>
+
+          <input type='password' placeholder='Password *' />
+
+          <div className='icon'>
+            <i class="fa-solid fa-eye-slash"></i>
+
+          </div>
+        </div>
+
+      </div>
+
+      <button>Signin</button>
+
+
     </div>
   );
 }
